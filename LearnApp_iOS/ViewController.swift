b11a5fa0779b4a58f8bb110e4ae176081d71ec24
Kamil Strąk <kamilstrak@MacBook-Pro-Kamil.local>
2017-03-13 19:18:24 +0100

    Initial Commit

---
+++ LearnApp_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnApp_iOS
+//
+//  Created by Kamil Strąk on 13.03.2017.
+//  Copyright © 2017 Kamil Strąk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
